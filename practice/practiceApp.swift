af84727925dcef349e44889fd74f675a79376760
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-29 14:46:01 -0600

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Benjamin Rojo on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
