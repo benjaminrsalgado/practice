894ccb9c9edba46c854c9c77526a0ed8ddf0f2ac
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-29 15:11:28 -0600

    i practice ScrollView and List

--- practice/ContentView.swift
+++ practice/ContentView.swift
@@ -14,8 +14,41 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            TaskListView()
+            ScrollView {
+                LazyVStack {
+                    ForEach(1...1000, id: \.self) { num in
+                        Text("Item \(num)")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.purple.opacity(0.2))
+                            .cornerRadius(10)
+                    }
+                }
+            }
         }
         .padding()
+    }
+}
+struct TaskListView: View {
+    let tasks = ["Do laundry", "Buy groceries", "Finish project"]
+
+    var body: some View {
+        List(tasks, id: \.self) { task in
+            HStack {
+                Text(task)
+                
+                Spacer()  // Empuja el botón a la derecha
+
+                Button(action: {
+                    print("\(task) completed!")
+                }) {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                }
+            }
+            .padding(.vertical, 8)
+        }
     }
 }
 
